e9a03910a747eb3b017a5a094c6b91b16cf56f61
Hilde Cabrera <hjca3120@gmail.com>
2025-04-02 05:59:24 -0400

    fixes

--- src/app/form/form.component.ts
+++ src/app/form/form.component.ts
@@ -1,63 +1,125 @@
 import { CommonModule } from '@angular/common';
-import { Component, OnInit } from '@angular/core';
-import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
-
+import {
+  Component,
+  ElementRef,
+  OnInit,
+  ViewChild,
+  viewChild,
+} from '@angular/core';
+import {
+  FormControl,
+  FormGroup,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
+import { MatDatepickerModule } from '@angular/material/datepicker';
+import { MatNativeDateModule } from '@angular/material/core';
 
 interface Itask {
   title: string;
   descrip: string;
-  complete: boolean
+  complete: boolean;
+  date?: Date;
+}
+
+interface Imessage {
+  main: string;
+  sub: string;
 }
 
 @Component({
   selector: 'app-form',
   standalone: true,
-  imports: [ReactiveFormsModule, CommonModule],
+  imports: [
+    ReactiveFormsModule,
+    CommonModule,
+    MatDatepickerModule,
+    MatNativeDateModule,
+  ],
   templateUrl: './form.component.html',
-  styleUrl: './form.component.css'
+  styleUrl: './form.component.css',
 })
 export class FormComponent implements OnInit {
+  @ViewChild('titleInput') titleInput!: ElementRef;
   taskForm: any = FormGroup;
+  picker: any;
 
   ngOnInit() {
     this.taskForm = new FormGroup({
       title: new FormControl('', [Validators.required]),
       descrip: new FormControl('', [Validators.required]),
+      date: new FormControl({value:null, disabled: false}),
     });
   }
 
-  tasks: Itask[] = [
-    { title: 'Tarea 1', descrip: 'Descripción de la tarea 1', complete: false },
-    { title: 'Tarea 2', descrip: 'Descripción de la tarea 2', complete: false },
-    { title: 'Tarea 3', descrip: 'Descripción de la tarea 3', complete: false }
-  ];
+  tasks: Itask[] = [];
   completedtasks: Itask[] = [
     { title: 'Tarea 4', descrip: 'Descripción de la tarea 4', complete: true },
-    { title: 'Tarea 5', descrip: 'Descripción de la tarea 5', complete: true }
+    { title: 'Tarea 5', descrip: 'Descripción de la tarea 5', complete: true },
   ];
-  showForm: boolean = false;
 
+  showForm: boolean = false;
   toggleForm() {
     this.showForm = !this.showForm;
+    this.focusInput();
+  }
+
+  focusInput() {
+    if (this.showForm) {
+      setTimeout(() => {
+        this.titleInput.nativeElement.focus();
+      });
+    }
+  }
+
+ 
+  selectedDateType: 'today' | 'tomorrow' | 'calendar' | null = null;
+
+  setTaskDate(type: 'today' | 'tomorrow' | 'calendar') {
+
+    if(this.selectedDateType === type){
+      this.selectedDateType = null;
+      this.taskForm.get('date')?.setValue(null);
+      return
+    }
+
+    const today = new Date();
+    let selectedDate = new Date(today)
+
+    if(type === 'tomorrow'){
+      selectedDate.setDate(today.getDate() + 1);
+    }
+
+    this.selectedDateType = type;
+    this.taskForm.get('date')?.setValue(selectedDate);
+
+    console.log(this.taskForm.get('date').value)
   }
 
   resettasks() {
     this.taskForm.reset();
+    this.selectedDateType = null;
   }
 
   pushtasks() {
-    if (this.taskForm.get('title').valid || this.taskForm.get('descrip').valid) {
+    if (
+      this.taskForm.get('title').valid ||
+      this.taskForm.get('descrip').valid
+    ) {
       const newtask: Itask = {
         title: this.taskForm.value.title,
         descrip: this.taskForm.value.descrip,
-        complete: false
+        complete: false,
+        date: this.taskForm.value.date,
       };
-    this.tasks.push(this.taskForm.value);
-    this.resettasks();
-    this.toggleForm();
+      this.tasks.push(this.taskForm.value);
+      console.log(this.taskForm.value);
+      this.resettasks();
+      this.toggleForm();
+      
     }
   }
-  
+
   estateCheck(task: Itask) {
     task.complete = !task.complete;
     if (task.complete) {
@@ -82,7 +144,7 @@ export class FormComponent implements OnInit {
       this.tasks.splice(indexInTasks, 1);
       return;
     }
-  
+
     // Si no está en tasks, buscar en completedtasks
     const indexInCompleted = this.completedtasks.indexOf(task);
     if (indexInCompleted > -1) {
@@ -90,4 +152,30 @@ export class FormComponent implements OnInit {
     }
   }
 
+  showMessage(): Imessage {
+    if (this.tasks.length === 0 && this.completedtasks.length === 0) {
+      return {
+        main: 'Aun no hay tareas',
+        sub: 'Agrega tareas para llevar un seguimiento de ellas',
+      };
+    } else if (this.tasks.length === 0 && this.completedtasks.length > 0) {
+      return {
+        main: 'Se han completado todas las tareas',
+        sub: '¡Buen trabajo!',
+      };
+    }
+    return {
+      main: '',
+      sub: '',
+    };
+  }
+
+  showCompletedTask: boolean = false;
+  toogleCompletedTask() {
+    this.showCompletedTask = !this.showCompletedTask;
+  }
+
+  completedTaskCount(): number {
+    return this.completedtasks.length;
+  }
 }
